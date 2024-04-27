ec44a3bd9da0f75508817470c91b7e4e385ffcec
Alex Paca <fujorita772@gmail.com>
2024-04-27 14:09:55 -0400

    adding console Answers

--- LastPractice/Parsing.hs
+++ LastPractice/Parsing.hs
@@ -12,15 +12,26 @@ Write a function parseTable :: [String] -> Table that parses a table
 represented in its concrete syntax as a list of strings (each corresponding to a single line
 in the input) into its abstract syntax. (Hint: use the function words from the Prelude.)
 -}
+
+{-
+ghci> parseTable ["hola","como","estas"]
+[["hola"],["como"],["estas"]]
+-}
+
 parseTable :: [String] -> Table
 parseTable [] = []
-parseTable (x:xs) = words x : map words xs
+parseTable xs = map words xs
 
 {-
 Exercise 3.1.2. Write a function printLine :: [Int] -> String that, given a list of
 widths of columns, returns a string containing a horizontal line. For example, printLine [5, 6, 6, 6]
 should return the line "+-----+------+------+------+". (Hint: use the function
 replicate.)
+-}
+
+{-
+ghci> printLine [5,6,6,6]
+"+-----+------+------+------+"
 -}
 
 printLine :: [Int] -> String
@@ -38,6 +49,14 @@ should return the formatted row
 "|Alice|Allen |female| 82000|"
 (Hint: use the functions intercalate, map and uncurry.)
 -}
+
+
+{-
+ghci> printRow [(1,"Anton"),(2,"Marisol"),(3,"Poma")]
+"|Anton|Marisol|Poma|"
+-}
+
+
 printRow :: [(Int, String)] -> String
 printRow fields = "|" ++ intercalate "|" (formFields fields) ++ "|"
 
@@ -50,6 +69,13 @@ formFields = map formField
 Exercise 3.1.6. Write a function printTable :: Table -> [String] that pretty prints the
 whole table. (Hint: use the functions map, toUpper and zip.)
 -}
+
+
+{-
+ghci> printTable [["row1","row1"],["row2","row2"]]
+["+----+----+","|ROW1|ROW1|","+----+----+","|row2|row2|","+----+----+"]
+-}
+
 printTable :: Table -> [String]
 printTable table =
     let (cells:rows) = table
@@ -59,11 +85,20 @@ printTable table =
         dataRows = dataRow rows columnWidths
     in [separatorLine, headerRow, separatorLine] ++ dataRows ++ [separatorLine]
 
+{-
+ghci> dataRow [["head1","head2"],["ho","la"]] [1,2]
+["|head1|head2|","|ho|la|"]
+-}
+
 dataRow :: [Row]->[Int] -> [String]
 dataRow [] _ = []
 dataRow (row:rows) columnWidths = printRow (zip columnWidths row) : dataRow rows columnWidths
 
-  
+{-
+ghci> headRow ["hola","ho"] [1,2]
+"|HOLA|HO|"
+-}
+
 headRow :: Row -> [Int] -> String
 headRow r col = printRow (zip col (map (map toUpper) r))
 
